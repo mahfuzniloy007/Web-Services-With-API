93cf2ed9d5a13d3e8391dd4f032febd33bd3c351
Mahfuz Ahmed <mahfuzniloy007@gmail.com>
2024-11-18 19:01:52 -0700

    1st commit

--- first_node_app/app.js
+++ first_node_app/app.js
@@ -1,6 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000 || 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method}, ${req.url}`);
+    next();
+});
+
+app.get("/user/:id", (req, res) => {
+    const userID = req.params.id;
+    res.send(`User id is ${userID}`);
+});
+
+app.get("/search", (req, res) => {
+    const query = req.params.q;
+    res.send(`Search query is: ${query}`);
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Connected to port ${port}!`)
+});
